2e5b3d6b98a207118afe2046ad2df1dadf130d6a
Antonina1909 <antonina1909@yandex.ru>
2023-02-05 07:17:14 +0500

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './index.scss';
+import './index.css';
+import { Success } from './components/Success';
+import { Books } from './components/Books';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [books, setBooks] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    //fetch('http://localhost:3001/data')
+    fetch('https://reqres.in/api/users')
+    .then((res) => res.json())
+    .then((json) => {
+      setBooks(json.data);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert('Ошибка при получении данных');
+    })
+    .finally(()=>setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter(_id => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {success ? (
+      <Success count={invites.length}/>
+    ):(
+      <Books 
+      onChangeSearchValue={onChangeSearchValue}
+      searchValue={searchValue} 
+      items = {books} 
+      isLoading={isLoading}
+      invites={invites}  
+      onClickInvite={onClickInvite}
+      onClickSendInvites={onClickSendInvites}
+      />
+    )}
     </div>
   );
 }
